1ffe748e654a35d72b38fcb06c3e9d0f3333a2ef
Unkown <Unkown@TWISTER>
2016-06-14 08:58:37 +0200

    all work

--- Blog.DAL.Tests/RepositoryTests.cs
+++ Blog.DAL.Tests/RepositoryTests.cs
@@ -18,20 +18,39 @@ namespace Blog.DAL.Tests
         {
             // arrange            
             var context = new BlogContext();
-
             context.Database.CreateIfNotExists();
 
+            // -- prepare data in db
+            context.Posts.ToList().ForEach(x => context.Posts.Remove(x));
+            context.Posts.Add(new Post
+            {
+                Author = "NewUSER",
+                Content = "test2, test2, test2..."
+            });
+
+            context.Posts.Add(new Post { Author = "MAX", Content = "TestContent", Id=3 });
+            context.Comments.Add(new Comment { Author = "max", Content = "cos fajnego", Id = 1, PostId = 3, Title = "New Comm" });
+
+            context.Posts.Add(new Post { Author = "Maksym", Content = "TestContent2", Id = 4 });
+            context.Comments.Add(new Comment { Author = "Mirek", Content = "Bardzo fajny i ciekawy post, polecam", Id = 41, PostId = 4, Title = "New Comm2" });
+            context.Comments.Add(new Comment { Author = "Krzycho", Content = "Polecam ten post", Id = 42, PostId = 4, Title = "Podpinam sie" });
+
+            context.SaveChanges();
+        }
+
             //initialize data
-            Post newPost = new Post("lalalalallala", "Damian");
-            Comment coment1 = new Comment("fajny", "bardzo fajny post", "zbychu", 9);
-            Comment coment2 = new Comment("polecam", "polecam Allegrowicza", "Rudy102", 9);
+            /*
+            Post newPost = new Post("This is the Fine POST", "Maksym Awdiejew");
+            Comment coment1 = new Comment("FINE POSTE", "Bardzo fajny i ciekawy post, polecam", "Krzysztof", 9);
+            Comment coment2 = new Comment("Podpinam sie", "Polecam ten post", "Krzycho", 9);
 
             var repository = new BlogRepository();
 
-            //repository.SavePost(newPost);
-            //repository.SaveComment(coment1);
-            //repository.SaveComment(coment2);
-        }
+            repository.SavePost(newPost);
+            repository.SaveComment(coment1);
+            repository.SaveComment(coment2);
+        }*/
+
         [TestMethod]
         public void IsDatabaseExist()
         {
@@ -40,7 +59,13 @@ namespace Blog.DAL.Tests
             Assert.IsTrue(result);
         }
 
-
+       /* [TestMethod]
+        public void DataBaseIsEmpty()
+        {
+            var context = new BlogContext();
+            Assert.IsNull(context);
+        }*/
+        
         [TestMethod]
         public void GetAllPost_OnePostInDb_ReturnOnePost()
         {
@@ -48,29 +73,29 @@ namespace Blog.DAL.Tests
             // act
             var result = repository.GetAllPosts();
             // assert
-            Assert.AreEqual(2, result.Count());
+            Assert.AreEqual(3, result.Count());
         }
-
+        
         [TestMethod]
         public void GetSpecificPostById()
         {
             var repository = new BlogRepository();
             // act
             var result = repository.GetAllPosts();
-            var post = result.Where(x => x.Id == 2);
+            var post = result.Where(x => x.Id == 1);
             // assert
             Assert.AreEqual(0, post.Count());
         }
-
+        
         [TestMethod]
         public void GetCommentsAssingToSpecificPost()
         {
             var repository = new BlogRepository();
             // act
             var coments = repository.GetAllComments();
-            var result = coments.Where(x => x.PostId == 9);
+            var result = coments.Where(x => x.PostId == 4);
             // assert
-            Assert.AreEqual(2, result.Count());
+            Assert.AreNotEqual(2, result.Count());
         }
 
         [TestMethod]
